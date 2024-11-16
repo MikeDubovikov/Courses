700018165046e4304eebffbe853f117e49cda1fd
Mikhail Dubovikov <dubovikov94@gmail.com>
2024-11-17 00:28:50 +0300

    Creating the favorites screen and minor fix main screen

--- presentation/src/main/java/com/mdubovikov/presentation/favorites_screen/FavoritesFragment.kt
+++ presentation/src/main/java/com/mdubovikov/presentation/favorites_screen/FavoritesFragment.kt
@@ -1,5 +1,29 @@
 package com.mdubovikov.presentation.favorites_screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mdubovikov.presentation.databinding.FragmentFavoritesBinding
 
-class FavoritesFragment : Fragment() {}
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding: FragmentFavoritesBinding
+        get() = _binding ?: throw IllegalStateException("Fragment $this binding cannot be accessed")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
