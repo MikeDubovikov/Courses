6b9f270d0448f3351e3d16b9e97821a64f0b1f45
Mikhail Dubovikov <dubovikov94@gmail.com>
2024-11-17 03:26:34 +0300

    Creating the details screen

--- presentation/src/main/java/com/mdubovikov/presentation/details_screen/DetailsFragment.kt
+++ presentation/src/main/java/com/mdubovikov/presentation/details_screen/DetailsFragment.kt
@@ -1,5 +1,29 @@
 package com.mdubovikov.presentation.details_screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mdubovikov.presentation.databinding.FragmentDetailsBinding
 
-class DetailsFragment : Fragment() {}
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding: FragmentDetailsBinding
+        get() = _binding ?: throw IllegalStateException("Fragment $this binding cannot be accessed")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
--- presentation/src/main/java/com/mdubovikov/presentation/profile_screen/ProfileFragment.kt
+++ presentation/src/main/java/com/mdubovikov/presentation/profile_screen/ProfileFragment.kt
@@ -1,5 +1,29 @@
 package com.mdubovikov.presentation.profile_screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.mdubovikov.presentation.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {}
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding: FragmentProfileBinding
+        get() = _binding ?: throw IllegalStateException("Fragment $this binding cannot be accessed")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
